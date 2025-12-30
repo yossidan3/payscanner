09ce15a4235b629544b574dd82d5140af929a838
yossidan3 <66twngzth7@privaterelay.appleid.com>
2025-12-30 13:30:16 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Payscanner.net</h1>
+      <p>
+        Compare acquiring banks for eCommerce & iGaming
+      </p>
+      <p>
+        Find the best approval rates, fees and countries coverage
+      </p>
+    </main>
+  );
+}
